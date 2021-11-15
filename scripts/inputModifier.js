329ff16808bac5c6574a89e7562f2a8a1088ad52
myrhanda <80593909+myrhanda@users.noreply.github.com>
2021-11-15 14:26:12 +0100

    ajout gestion tenues

--- scripts/inputModifier.js
+++ scripts/inputModifier.js
@@ -6,6 +6,7 @@ const WORLD_INFO_KEY_REGEX_PREFIX = '(?:^\\s*|,\\s*)(?<key>'
 const WORLD_INFO_KEY_REGEX_SUFFIX = ')(?:_(?<digits>\\d+))?(?:\\s*,|\\s*$)'
 
 const PROMPT_KEY = 'myrha_prompt'
+const DRESS_KEY = 'myrha_dress'
 
 const getWorldInfoKeyRegex = (key) => {
   return RegExp(WORLD_INFO_KEY_REGEX_PREFIX + key + WORLD_INFO_KEY_REGEX_SUFFIX, 'i')
@@ -16,22 +17,71 @@ const getWorldInfoEntriesByKey = (key) => {
   return worldEntries?.filter(entry => entry.keys.match(regex))
 }
 
-const getRandomPrompt = () => {
+const getRandomPromptInfo = () => {
   const regex = getWorldInfoKeyRegex(PROMPT_KEY)
   const promptInfoList = worldInfo?.filter(entry => entry.type.match(regex))
   const index = Math.floor((Math.random() * promptInfoList.length));
   let result = ""
   if (index < promptInfoList.length){
-    const promptInfo = promptInfoList[index];
-    result=promptInfo.entry
-    if(result == undefined){
-      result = ""
-    }
-    else{
-      result = "\n"+result
-    }
+    result = promptInfoList[index];
   }
   return result
+}
+
+const composeFinalPrompt=(promptText, promptWear)=>{
+  //return "compose: text="+promptText+" wear="+promptWear
+	if(promptText && promptWear){
+		return promptWear+"\n"+promptText
+	}
+	else{
+		return promptWear+""+promptText
+	}
+}
+
+const getRandomPrompt = () => {
+	//extraire le worldInfo
+	const promptInfo = getRandomPromptInfo()
+	let promptText = ""
+	let promptWear = ""
+	if(promptInfo != undefined){
+		//prompt
+		if(promptInfo.entry != undefined){
+			promptText = promptInfo.entry
+		}
+		//wear pour ce prompt
+		const attributes=promptInfo.attributes
+		if(attributes){
+			let dress 
+			if(attributes.myrha_dress){
+			  dress=attributes.myrha_dress
+			}
+			if(dress){
+				promptWear = getRandomDress(dress)
+			}
+		}
+	}
+	return composeFinalPrompt(promptText, promptWear)
+}
+
+const getRandomDress=(key) =>{
+	const dressInfo = getRandomDressInfo(key)
+	let result=""
+	if(dressInfo){
+	  result=dressInfo.entry
+	}
+	return result
+}
+
+const getRandomDressInfo=(key) =>{
+	const dressRegex = getWorldInfoKeyRegex(DRESS_KEY)
+	const specificRegex = getWorldInfoKeyRegex(key)
+	const promptInfoList = worldInfo?.filter(entry => entry.type.match(dressRegex))?.filter(entry => entry.type.match(specificRegex))
+	const index = Math.floor((Math.random() * promptInfoList.length));
+	let result = ""
+	if (index < promptInfoList.length){
+		result = promptInfoList[index];
+	}
+	return result
 }
 
 const modifier = (text) => {
